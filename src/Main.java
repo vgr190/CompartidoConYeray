267fcedee8a6727ba8f307c5be9c6cc52da33127
Victor <vgr190@educa.madrid.org>
2025-12-04 09:37:05 +0100

    Los popochos estan placidamente

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,6 @@ public class Main {
 
         System.out.println("el popocho " +(i+1) +" está ZZZzzz");
         }
-        System.out.println("Todos los popochitos están durmiendo");
+        System.out.println("Todos los popochitos están durmiendo placidamente");
     }
 }
