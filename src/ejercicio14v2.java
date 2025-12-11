216d54d987a0064ff0cab0951f129ef25080a777
Victor <vgr190@educa.madrid.org>
2025-12-11 09:57:07 +0100

    programa para practica en grupo

---
+++ src/ejercicio14v2.java
@@ -0,0 +1,34 @@
+
+
+
+import java.util.Scanner;
+
+    public class ejercicio14v2 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+
+            //14.	Programa que lea de forma repetida un número N. Para cada número leído calculará
+            //y mostrará la suma 1+2+3+...+N. Cada vez que muestre el resultado el programa preguntará
+            //al usuario si desea continuar,
+            //si se introduce s el programa continuará. En caso contrario finalizará.
+
+
+            char respuesta;
+            do {
+                System.out.println("introduce el numero");
+                int n = scan.nextInt();
+                scan.nextLine();   //limpiar el buffer
+                int suma=0;
+                for(	;n>=1;n--) {
+                    suma += n;
+                }
+                System.out.println("La suma es: "+suma);
+
+                System.out.println("Desea continuar(s/n)");
+                respuesta= scan.nextLine().charAt(0);
+
+            }while(respuesta == 's');
+
+        }
+
+}
